ac266e94b84a4a8a5bc023d2e8e869f5edb4e30f
joypaul <jpaul3250@gmail.com>
2019-06-10 20:38:27 -0400

    fixed crash on join alert due to being offline

--- litterly/View Controllers/AlertVCs/JoinAlertVC/JoinAlertViewController.swift
+++ litterly/View Controllers/AlertVCs/JoinAlertVC/JoinAlertViewController.swift
@@ -112,26 +112,43 @@ class JoinAlertViewController: UIViewController, UICollectionViewDelegate, UICol
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "PeopleAttendingCollectionViewCell", for: indexPath) as! PeopleAttendingCollectionViewCell
         
-        guard let usersData = confirmedUsers else {return cell}
-        
         //gets image from network.
-        // TODO := need to provide default values so it doesnt return nil and crash
-        let userImage = usersData[indexPath.row]["user_pic_url"] as! String
+        let userImage = confirmedUsers[indexPath.row]["user_pic_url"] as! String
         let imageUrl = URL(string: userImage)
-        let data = try! Data(contentsOf: imageUrl!)
-        let image = UIImage(data: data)
-        cell.attendingUserProfileImages.image = image
         
-        //rounds and configures the cell
-        cell.attendingUserProfileImages.contentMode = .scaleAspectFill
-        cell.attendingUserProfileImages.isOpaque = false
-        cell.attendingUserProfileImages.layer.borderWidth = 1
-        cell.attendingUserProfileImages.layer.masksToBounds = false
-        cell.attendingUserProfileImages.layer.borderColor = UIColor.searchBoxTextGray.cgColor
-        cell.attendingUserProfileImages.layer.cornerRadius = cell.attendingUserProfileImages.frame.height/2
-        cell.attendingUserProfileImages.clipsToBounds = true
+        if let data = try? Data(contentsOf: (imageUrl)!){
+            let image = UIImage(data: data)
+            
+            cell.attendingUserProfileImages.image = image
+            //rounds and configures the cell
+            cell.attendingUserProfileImages.contentMode = .scaleAspectFill
+            cell.attendingUserProfileImages.isOpaque = false
+            cell.attendingUserProfileImages.layer.borderWidth = 1
+            cell.attendingUserProfileImages.layer.masksToBounds = false
+            cell.attendingUserProfileImages.layer.borderColor = UIColor.searchBoxTextGray.cgColor
+            cell.attendingUserProfileImages.layer.cornerRadius = cell.attendingUserProfileImages.frame.height/2
+            cell.attendingUserProfileImages.clipsToBounds = true
+            
+            return cell
+            
+        } else{
+            
+            //if downloaded image is nil, provide a default value
+            cell.attendingUserProfileImages.image = UIImage(named: "profile")
+            //rounds and configures the cell
+            cell.attendingUserProfileImages.contentMode = .scaleAspectFill
+            cell.attendingUserProfileImages.isOpaque = false
+            cell.attendingUserProfileImages.layer.borderWidth = 1
+            cell.attendingUserProfileImages.layer.masksToBounds = false
+            cell.attendingUserProfileImages.layer.borderColor = UIColor.searchBoxTextGray.cgColor
+            cell.attendingUserProfileImages.layer.cornerRadius = cell.attendingUserProfileImages.frame.height/2
+            cell.attendingUserProfileImages.clipsToBounds = true
+            
+            return cell
+        }
         
-        return cell
+        //return cell
+        
     }
     
     //animates the cells appearing
