469244238c75d423cc01fd7387ab215f51df01bd
joypaul <jpaul3250@gmail.com>
2019-06-18 20:05:33 -0400

    created files for nearby

---
+++ litterly/View Controllers/NearbyVC/NearbyViewController.swift
@@ -0,0 +1,30 @@
+//
+//  NearbyViewController.swift
+//  litterly
+//
+//  Created by Joy Paul on 6/18/19.
+//  Copyright © 2019 Joy Paul. All rights reserved.
+//
+
+import UIKit
+
+class NearbyViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
