59643160db64feb825abf1b52e07d18666812922
joypaul <jpaul3250@gmail.com>
2019-07-09 21:04:00 -0400

    programatic approach

--- litterly/View Controllers/NearbyVC/NearbyViewController.swift
+++ litterly/View Controllers/NearbyVC/NearbyViewController.swift
@@ -10,65 +10,86 @@ import UIKit
 
 class NearbyViewController: UIViewController {
     
-    @IBOutlet weak var meetupsCollectionView: UICollectionView!
-    @IBOutlet weak var trashCollectionView: UICollectionView!
-    
-    var meetups = MeetUps.createMeetUps()
-    var trashTag = TrashTags.createTrashTags()
- 
+    @IBOutlet var parentView: UIView!
+    let trashTagsView = UIView()
+    let meetupsView = UIView()
+    let trashTagCollectionView = UICollectionView()
+    let meetupsCollectionView = UICollectionView()
+
     override func viewDidLoad() {
-        super.viewDidLoad()
-        trashCollectionView.dataSource = self
-        meetupsCollectionView.dataSource = self
+        configureNavbar()
+        configureParentViews()
+//        trashTagCollectionView.dataSource = self
+//        trashTagCollectionView.delegate = self
+    }
+    
+    func configureNavbar(){
         
-        configureColors()
+        //removes the bar border color
+        navigationController?.navigationBar.setBackgroundImage(UIImage(), for: UIBarMetrics.default)
+        navigationController?.navigationBar.shadowImage = UIImage()
+        
+        navigationController?.navigationBar.barTintColor = UIColor.mainBlue
+        
+        let textAttributes = [NSAttributedString.Key.foregroundColor:UIColor.textWhite, NSAttributedString.Key.font: UIFont(name: "MarkerFelt-Thin", size: 40)]
+        navigationController?.navigationBar.largeTitleTextAttributes = textAttributes as [NSAttributedString.Key : Any]
+        navigationController?.navigationBar.prefersLargeTitles = true
+        navigationItem.title = "Nearby"
+        navigationController?.navigationBar.backgroundColor = UIColor.mainBlue
+        
+        // create the button
+        let backImage  = UIImage(named: "white_back_arrow")!.withRenderingMode(.alwaysOriginal)
+        let backButton = UIButton(frame: CGRect(x: 0, y: 0, width: 30, height: 30))
+        backButton.setBackgroundImage(backImage, for: .normal)
+        //backButton.addTarget(self, action: #selector(goBackToMap), for: .touchUpInside)
+        
+        // here where the magic happens, you can shift it where you like
+        backButton.transform = CGAffineTransform(translationX: 10, y: 0)
+        
+        // add the button to a container, otherwise the transform will be ignored
+        let backButtonContainer = UIView(frame: backButton.frame)
+        backButtonContainer.addSubview(backButton)
+        let leftNavButtonItem = UIBarButtonItem(customView: backButtonContainer)
+        
+        // add button shift to the side
+        navigationItem.leftBarButtonItem = leftNavButtonItem
+    }
+    
+    //back button onTap() func
+    @objc func goBackToMap(){
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    func configureParentViews(){
+        
+        trashTagsView.frame = CGRect(x: 0, y: 0, width: parentView.frame.width, height: parentView.frame.height/2)
+        parentView.addSubview(trashTagsView)
+        
+        trashTagsView.translatesAutoresizingMaskIntoConstraints = false
+        trashTagsView.topAnchor.constraint(equalTo: parentView.safeAreaLayoutGuide.topAnchor).isActive = true
+        trashTagsView.leftAnchor.constraint(equalTo: parentView.leftAnchor).isActive = true
+        trashTagsView.rightAnchor.constraint(equalTo: parentView.rightAnchor).isActive = true
+        trashTagsView.heightAnchor.constraint(equalToConstant: parentView.frame.height / 2 - (self.navigationController?.navigationBar.frame.size.height)!).isActive = true
+        trashTagsView.backgroundColor = .yellow
+        
+        
+        meetupsView.frame = CGRect(x: 0, y: 0, width: self.view.frame.width, height: self.view.frame.height / 2)
+        parentView.addSubview(meetupsView)
+        
+        meetupsView.translatesAutoresizingMaskIntoConstraints = false
+        meetupsView.topAnchor.constraint(equalTo: trashTagsView.bottomAnchor).isActive = true
+        meetupsView.leftAnchor.constraint(equalTo: self.view.leftAnchor).isActive = true
+        meetupsView.rightAnchor.constraint(equalTo: self.view.rightAnchor).isActive = true
+        meetupsView.bottomAnchor.constraint(equalTo: self.view.bottomAnchor).isActive = true
+        meetupsView.backgroundColor = .green
         
     }
     
-    func configureColors(){
-        self.view.backgroundColor = UIColor.mainBlue
-        meetupsCollectionView.backgroundColor = UIColor.mainBlue
-        trashCollectionView.backgroundColor = UIColor.mainBlue
+    //todo: configure the respective collectionViews
+    func conFigureCollectionViews(){
+        
     }
+  
 }
 
 
-extension NearbyViewController: UICollectionViewDataSource{
-    
-    func configureCollectionViewSize(){
-        let width = (view.frame.size.width) / 2.5
-        let layout = trashCollectionView.collectionViewLayout as! UICollectionViewFlowLayout
-        
-        layout.itemSize = CGSize(width: width, height: width)
-    }
-    
-    func numberOfSections(in collectionView: UICollectionView) -> Int {
-        return 1
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-
-        if collectionView == self.trashCollectionView {
-            return trashTag.count
-        }
-        
-        return  meetups.count
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        
-        if collectionView == self.trashCollectionView {
-            
-            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "TrashTagsCollectionViewCell", for: indexPath) as! TrashTagsCollectionViewCell
-            cell.trashTag = trashTag[indexPath.item]
-            return cell
-        }
-            
-        else {
-            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "MeetupsCollectionViewCell", for: indexPath) as! MeetupsCollectionViewCell
-            cell.meetup = meetups[indexPath.item]
-            return cell
-        }
-        
-    }
-}
--- litterly/Views/Cells/TrashTagsCollectionViewCell.swift
+++ litterly/Views/Cells/TrashTagsCollectionViewCell.swift
@@ -10,34 +10,7 @@ import UIKit
 
 class TrashTagsCollectionViewCell: UICollectionViewCell {
     
-    @IBOutlet weak var featuredImageView: UIImageView!
-    @IBOutlet weak var overlayView: UIView!
-    @IBOutlet weak var descriptionLabel: UILabel!
-    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var nearbyTrashTagCollectionViewCell: UIImageView!
     
-    var trashTag: TrashTags! {
-        didSet {
-            self.updateUI()
-        }
-    }
-    
-    func updateUI() {
-        if let trashTag = trashTag {
-            featuredImageView.image = trashTag.featuredImage
-            titleLabel.text = trashTag.title
-            descriptionLabel.text = trashTag.description
-            overlayView.backgroundColor = trashTag.color
-        } else {
-            featuredImageView.image = nil
-            titleLabel.text = nil
-            descriptionLabel.text = nil
-            overlayView.backgroundColor = nil
-        }
-        
-        featuredImageView.layer.cornerRadius = 10.0
-        featuredImageView.layer.masksToBounds = true
-        overlayView.layer.cornerRadius = 10.0
-        overlayView.layer.masksToBounds = true
-        overlayView.layer.maskedCorners = [.layerMaxXMaxYCorner, .layerMinXMaxYCorner]
-    }
+
 }
